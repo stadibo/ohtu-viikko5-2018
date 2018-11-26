01c779f69f24c92d1a383bb326f087a6aa30d857
stadibo <jesperleonard@gmail.com>
2018-11-26 22:48:14 +0200

    Week5 e2 complete

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -2,79 +2,104 @@ package ohtu;
 
 public class TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int playerOneScore = 0;
+    private int playerTwoScore = 0;
+    private String playerOneName;
+    private String playerTwoName;
 
     public TennisGame(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.playerOneName = player1Name;
+        this.playerTwoName = player2Name;
     }
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            playerOneScore++;
         else
-            m_score2 += 1;
+            playerTwoScore++;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
+        if (scoreIsEven()) { 
+            return printEvenScore(); 
+        } else if (playerHasAdvantageOrWinsPoint()) {
+            if (playerHasAdvantage()) {
+                return printAdvantage();
+            } else {
+                return printWinnerForPoint();
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        else {
+            return printCurrentScore();
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        
+    }
+
+    private int scoreDifference() {
+        return playerOneScore - playerTwoScore;
+    }
+    
+    private String printCurrentScore() {
+        return getScoreName(playerOneScore) + "-" + getScoreName(playerTwoScore);
+    }
+
+    private String getScoreName(int score) {
+        switch(score) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return "";
         }
-        return score;
+    }
+
+    private String printWinnerForPoint() {
+        if (playerOneWins()) return "Win for player1";
+        else return "Win for player2";
+    }
+
+    private boolean playerOneWins() {
+        return scoreDifference() >= 2;
+    }
+
+    private String printAdvantage() {
+        if (playerOneHasAdvantage()) return "Advantage player1";
+        else return "Advantage player2";
+    }
+
+    private boolean playerHasAdvantageOrWinsPoint() {
+        return playerOneScore >= 4 || playerTwoScore >= 4;
+    }
+    
+    private boolean playerHasAdvantage() {
+        return Math.abs(scoreDifference()) == 1;
+    }
+    
+    private boolean playerOneHasAdvantage() {
+        return scoreDifference() == 1;
+    }
+
+    private boolean scoreIsEven() {
+        return playerOneScore == playerTwoScore;
+    }
+
+    private String printEvenScore() {
+        switch (playerOneScore) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            case 3:
+                return "Forty-All";
+            default:
+                return "Deuce";       
+        }
     }
 }
